e6b033553068581cb38ec840633007412ffbabdf
Jobin Thomas <jobinthomas@nypl.org>
2017-03-21 14:37:04 -0400

    Externalize values in config.json file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-console.log("Loading sierra bib retriever");
+console.log("Loading sierra resource retriever service");
 
 const avro = require('avsc');
 const AWS = require('aws-sdk')
 const crypto = require('crypto')
 const kinesis = new AWS.Kinesis({region: 'us-east-1'})
 const wrapper = require('sierra-wrapper')
+const fs = require('fs')
+
+var schema_data = null;
+var stream = null;
 
 //main function
 exports.handler = function(event, context){
@@ -17,36 +21,61 @@ exports.handler = function(event, context){
 //kinesis stream handler
 var kinesisHandler = function(records, context) {
   console.log('Processing ' + records.length + ' records');
-  var url = "https://api.nypltech.org/api/v0.1/current-schemas/SierraItemRetrievalRequest";
-  schema(url)
-    .then(function(schema_data) {
-      records.forEach(function(record){
-        var data = record.kinesis.data;
-        var json_data = avro_decoded_data(schema_data, data);
-        bib_in_detail(json_data.id)
-          .then(function(bib) {
-            postBibsStream(bib);
-          })
+  fs.readFile('config.json')
+  configValsAsJSON('config.json', 'utf-8')
+  .then(function(jsonContents){
+    var schema_url = jsonContents.schema_url;
+    stream = jsonContents.stream;
+    schema(schema_url)
+      .then(function(schema_data) {
+        records.forEach(function(record){
+          var data = record.kinesis.data;
+          var json_data = avro_decoded_data(schema_data, data);
+          bib_in_detail(json_data.id)
+            .then(function(bib) {
+              postBibsStream(bib);
+            })
+        });
+      })
+      .catch(function(e){
+        console.log(e, e.stack);
       });
+  })
+}
+
+//get configuration values
+var configValsAsJSON = function(fileName, encoding){
+  return new Promise(function(resolve, reject){
+    fs.readFile(fileName, encoding, function(err, data){
+      if (err){
+        console.log(err, err.stack) // an error occurred
+        reject("Unable to read configuration file contents");
+      }
+      var jsonContents = JSON.parse(data);
+      resolve(jsonContents);
     })
-    .catch(function(e){
-      console.log(e);
-    });
+  })
 }
 
 //get schema
 var request = require('request');
 var schema = function(url, context) {
-  return new Promise(function(resolve, reject) {
-    request(url, function(error, response, body) {
-      if(!error && response.statusCode == 200){
-        resolve(JSON.parse(body).data.schema);
-      }else{
-        console.log("An error occurred - " + response.statusCode);
-        reject("Error occurred while getting schema - " + response.statusCode);
-      }
+  if(schema_data === null){
+    return new Promise(function(resolve, reject) {
+      console.log('Querying for schema - ' + url);
+      request(url, function(error, response, body) {
+        if(!error && response.statusCode == 200){
+          resolve(JSON.parse(body).data.schema);
+        }else{
+          console.log("An error occurred - " + response.statusCode);
+          reject("Error occurred while getting schema - " + response.statusCode);
+        }
+      })
     })
-  })
+  }else {
+    console.log('schema is already stored. Let\'s use that for processing');
+    return schema_data;
+  }
 }
 
 //use avro to deserialize
@@ -85,7 +114,7 @@ var postBibsStream = function(bib){
   var params = {
     Data: bib_in_avro_format, /* required */
     PartitionKey: crypto.randomBytes(20).toString('hex').toString(), /* required */
-    StreamName: 'testBibRetrieval', /* required */
+    StreamName: stream, /* required */
   }
   kinesis.putRecord(params, function (err, data) {
     if (err) console.log(err, err.stack) // an error occurred
