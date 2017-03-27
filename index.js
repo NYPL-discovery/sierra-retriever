d83655c1f6ed37a2298ada9906467898a197e11f
Jobin Thomas <jobinthomas@nypl.org>
2017-03-27 16:41:37 -0400

    Added code to use bib or item

--- index.js
+++ index.js
@@ -24,55 +24,69 @@ var kinesisHandler = function(records, context) {
   console.log('Processing ' + records.length + ' records');
   wrapper.loadConfig('./config/default.json');
   if(schema_stream_retriever === null){
-    schema(config.get('schema_retrieval_url'))
-    .then(function(schema_data){
-      schema_stream_retriever = schema_data;
-      if(wrapper_access_token === null){
-        set_wrapper_access_token()
-        .then(function(access_token){
-          console.log('Access token is - ' + access_token);
-          wrapper_access_token = access_token;
+    var schema_retrieval_url = null;
+    if(config.get('isABib')){
+     schema_retrieval_url = config.get('bib.schema_retrieval_url');
+    }else{
+      schema_retrieval_url = config.get('item.schema_retrieval_url');
+    }
+    schema(schema_retrieval_url)
+      .then(function(schema_data){
+        schema_stream_retriever = schema_data;
+        if(wrapper_access_token === null){
+            setGlobalAccessTokenAndProcessResources(records, schema_data);
+        }else{
           processResources(records, schema_data);
-        })
-      }else{
-        processResources(records, schema_data);
-      }
-    })
+        }
+      })
     .catch(function(e){
       console.log(e, e.stack);
     });
   }else{
     if(wrapper_access_token === null){
-      set_wrapper_access_token()
-      .then(function(access_token){
-        console.log('Access token is - ' + access_token);
-        wrapper_access_token = access_token;
-        processResources(records, schema_stream_retriever);
-      })
+      setGlobalAccessTokenAndProcessResources(records, schema_stream_retriever);
     }else{
       processResources(records, schema_stream_retriever);
     }
   }
 }
 
+var setGlobalAccessTokenAndProcessResources = (function(records, schema_data){
+  set_wrapper_access_token()
+        .then(function(access_token){
+          console.log('Access token is - ' + access_token);
+          wrapper_access_token = access_token;
+          processResources(records, schema_data);
+        });
+  });
+
 //process resources
 var processResources = function(records, schema_data){
   records.forEach(function(record){
       var data = record.kinesis.data;
       var json_data = avro_decoded_data(schema_data, data);
-      bib_in_detail(json_data.id)
+      if(config.get('isABib')){
+        resource_in_detail(json_data.id, true)
           .then(function(bib) {
-            postBibsStream(bib);
+            var stream = config.get('bib.stream');
+            postResourcesToStream(bib, stream);
           })
           .catch(function (e){
             console.log(e);
           });
+      }else{
+        resource_in_detail(json_data.id, false)
+          .then(function(item){
+            var stream = config.get('item.stream');
+            postResourcesToStream(item, stream);
+          })
+      }
     });
 }
 
 //get schema
 var request = require('request');
-var schema = function(url, context) {
+var schema = function(url) {
   return new Promise(function(resolve, reject) {
       console.log('Querying for schema - ' + url);
       request(url, function(error, response, body) {
@@ -94,8 +108,8 @@ var avro_decoded_data = function(schema_data, record){
   return JSON.parse(verify);
 }
 
-//get full bib details for each bib
-var bib_in_detail = function(bibId){
+//get full bib/item details for each bib/item id
+var resource_in_detail = function(id, isBib){
   return new Promise(function(resolve, reject){
     var operation = retry.operation({
         retries: 5,
@@ -105,28 +119,65 @@ var bib_in_detail = function(bibId){
         randomize: true,
     });
     operation.attempt(function(currentAttempt) {
-      wrapper.requestSingleBib(bibId, (errorBibReq, results) => {
-      if(errorBibReq){
-        if(errorBibReq.httpStatus !== null && errorBibReq.httpStatus === 401){
+      if(isBib){
+        console.log('Requesting for bib info');
+        wrapper.requestSingleBib(id, (errorBibReq, results) => {
+        getResult(errorBibReq, results, true, id, operation, currentAttempt)
+            .then(function(entry){
+              resolve (entry);
+            })
+            .catch (function(e) {
+              reject (e);
+            });
+        });
+      } else{
+        console.log('Requesting for item info');
+        var itemIds = [id];
+        wrapper.requestMultiItemBasic(itemIds, (errorItemReq, results) => {
+          getResult(errorItemReq, results, false, itemIds, operation, currentAttempt)
+            .then(function(entry){
+              resolve (entry);
+            })
+            .catch (function(e) {
+              reject (e);
+            });
+        });
+      }
+    });
+  });
+}
+
+//get bib or item based on prama passed
+var getResult = function(errorResourceReq, results, isBib, resourceId, operation, attemptNumber){
+  return new Promise(function(resolve, reject){
+    if(errorResourceReq){
+        if(errorResourceReq.httpStatus !== null && errorResourceReq.httpStatus === 401){
           console.log("This is a token issue. Going to renew token");
-          console.log('Number of re-attempts for bib ' + bibId + ' - ' + currentAttempt);
+          if(isBib) {
+            console.log('Number of attempts made for bib ' + resourceId + ' - ' + attemptNumber);
+          } else{
+            console.log('Number of attempts made for item ' + resourceId + ' - ' + attemptNumber);
+          }
           set_wrapper_access_token()
-          .then(function (access_token){
-            wrapper_access_token = access_token;
-            if(operation.retry(errorBibReq))
-              return;
-          });
+            .then(function (access_token){
+              wrapper_access_token = access_token;
+              if(operation.retry(errorResourceReq)){
+                return;
+                if(isBib){
+                  console.log('Error occurred while getting bib info');
+                } else {
+                  console.log('Error occurred while getting item info');
+                }
+                reject(errorResourceReq);
+              }
+            });
         }
-          console.log('Error occurred while getting bib info');
-          reject(errorBibReq);
-        } else {
+      } else {
             var entries = results.data.entries;
             var entry = entries[0];
             console.log(JSON.stringify(entry));
             resolve(entry);
           }
-      });
-    });
   });
 }
 
@@ -144,13 +195,13 @@ var set_wrapper_access_token = function(){
 }
 
 //send data to kinesis Stream
-var postBibsStream = function(bib){
-  const type = avro.infer(bib);
-  const bib_in_avro_format = type.toBuffer(bib);
+var postResourcesToStream = function(resource, stream){
+  const type = avro.infer(resource);
+  const resource_in_avro_format = type.toBuffer(resource);
   var params = {
-    Data: bib_in_avro_format, // required 
+    Data: resource_in_avro_format, // required 
     PartitionKey: crypto.randomBytes(20).toString('hex').toString(), // required
-    StreamName: config.get('stream'), // required 
+    StreamName: stream, // required 
   }
   kinesis.putRecord(params, function (err, data) {
     if (err) console.log(err, err.stack) // an error occurred
